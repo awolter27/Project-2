47a8f4b67106fc0a1243b8a093cb085f3384f97d
frostyserge <eyemoroz@gmail.com>
2023-04-11 23:16:47 -0400

    all three schemas are complete

---
+++ models/seedNetflix.js
@@ -0,0 +1,511 @@
+module.exports = [
+    {
+        name: "Somebody Feed Phil",
+        synopsis: "Phil travels around the world sampling food and tradition with friends and a sense of humor.",
+        img: "https://i1.wp.com/wineyparent.com/wp-content/uploads/2020/12/Somebody-Feed-Phil-feature-image-EDIT-AGAIN.png?w=882&ssl=1",
+        genre: ["docuseries", "lifestyle", "reality", "travel and adventure"],
+        seasons: {
+            one: {
+                year: 2018,
+                episodes: {
+                    one: "Bangkok",
+                    two: "Saigon",
+                    three: "Tel Aviv",
+                    four: "Lisbon",
+                    five: "New Orleans",
+                    six: "Mexico City",
+                }
+            },
+            two: {
+                year: 2018,
+                episodes: {
+                    one: "Venice",
+                    two: "Dublin",
+                    three: "Buenos Aires",
+                    four: "Copenhagen",
+                    five: "Cape Town",
+                    six: "New York City",
+                }
+            },
+            three: {
+                year: 2020,
+                episodes: {
+                    one: "Marrakesh",
+                    two: "Chicago",
+                    three: "London",
+                    four: "Seoul",
+                    five: "Montreal",
+                }
+            },
+            four: {
+                year: 2020,
+                episodes: {
+                    one: "Rio de Janeiro",
+                    two: "San Francisco",
+                    three: "Singapore",
+                    four: "The Mississippi Delta",
+                    five: "Hawaii",
+                }
+            },
+            five: {
+                year: 2022,
+                episodes: {
+                    one: "Oaxaca",
+                    two: "Maine",
+                    three: "Helsinki",
+                    four: "Portland",
+                    five: "Madrid",
+                }
+            },
+            six: {
+                year: 2022,
+                episodes: {
+                    one: "Philadelphia",
+                    two: "Croatia",
+                    three: "Austin",
+                    four: "Santiago de Chile",
+                    five: "Nashville",
+                    six: "Tribute to Helen and Max"
+                }
+            },
+        }
+    },
+    {
+        name: "The Last Kingdom",
+        synopsis: "Abducted by a Viking warlord after his father is slain in battle, young Utred adopts the Norse way of life until tragedy befalls his new family.",
+        img: "https://www.tvguide.com/a/img/catalog/provider/1/1/1-12206744520.jpg",
+        genre: ["british", "tv dramas", "based on books"],
+        seasons: {
+            one: {
+                year: 2015,
+                episodes: {
+                    one: "1.1",
+                    two: "1.2",
+                    three: "1.3",
+                    four: "1.4",
+                    five: "1.5",
+                    six: "1.6",
+                    seven: "1.7",
+                    eight: "1.8"
+                }
+            },
+            two: {
+                year: 2017,
+                episodes: {
+                    one: "2.1",
+                    two: "2.2",
+                    three: "2.3",
+                    four: "2.4",
+                    five: "2.5",
+                    six: "2.6",
+                    seven: "2.7",
+                    eight: "2.8"
+                }
+            },
+            three: {
+                year: 2018,
+                episodes: {
+                    one: "3.1",
+                    two: "3.2",
+                    three: "3.3",
+                    four: "3.4",
+                    five: "3.5",
+                    six: "3.6",
+                    seven: "3.7",
+                    eight: "3.8"
+                }
+            },
+            four: {
+                year: 2020,
+                episodes: {
+                    one: "4.1",
+                    two: "4.2",
+                    three: "4.3",
+                    four: "4.4",
+                    five: "4.5",
+                    six: "4.6",
+                    seven: "4.7",
+                    eight: "4.8",
+                    nine: "4.9",
+                    ten: "4.10"
+                }
+            },
+            five: {
+                year: 2022,
+                episodes: {
+                    one: "5.1",
+                    two: "5.2",
+                    three: "5.3",
+                    four: "5.4",
+                    five: "5.5",
+                    six: "5.6",
+                    seven: "5.7",
+                    eight: "5.8",
+                    nine: "5.9",
+                    ten: "5.10"
+                }
+            }
+        }
+    },
+    {
+        name: "The Playlist",
+        synopsis: "After achieving an audacious start-up goal, self-taught coder Daniel Ek takes on the challenge of solving the music industry's piracy issue.",
+        img: "https://occ-0-38-34.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZkgFwrRrkIvRqDC424kxXRw8ZxPJ0CI9mr5p5VZDBd21hD-2b5eL4K7U5g4CK65VR777-qIWl1m__ivDO-JW4HOZTWJ66UnM_6FSh7qQ7fwWukvSkR_gb3KSm5Ssq7KEoPC3g.jpg?r=fc0",
+        genre: ["swedish", "based on real life", "based on books", "tv dramas", "music"],
+        seasons: {
+            one: {
+                year: 2022,
+                episodes: {
+                    one: "The Vision",
+                    two: "The Industry",
+                    three: "The Law",
+                    four: "The Coder",
+                    five: "The Partner",
+                    six: "The Artist"
+                }
+            }
+        }
+    },
+    {
+        name: "Breaking Bad",
+        synopsis: "Diagnosed with terminal lung cancer, chemistry teacher Walter White teams up with former student Jesse Pinkman to cook and sell crystal meth.",
+        img: "https://static.boredpanda.com/blog/wp-content/uploads/2018/06/breaking-bad-anniversary-10-year-cast-reunion-photoshoot-5b361591b3476__880.jpg",
+        genre: ["crime tv shows", "tv thrillers", "tv dramas"],
+        seasons: {
+            one: {
+                year: 2008,
+                episodes: {
+                    one: "Pilot",
+                    two: "Cat's in the Bag...",
+                    three: "...And the Bag's in the River",
+                    four: "Cancer Man",
+                    five: "Gray Matter",
+                    six: "Crazy Handful of Nothin'",
+                    seven: "A No-Rough-Stuff-Type Deal"
+                }
+            },
+            two: {
+                year: 2009,
+                episodes: {
+                    one: "Seven Thirty-Seven",
+                    two: "Grilled",
+                    three: "Bit by a Dead Bee",
+                    four: "Down",
+                    five: "Breakage",
+                    six: "Peekaboo",
+                    seven: "Negro y Azul",
+                    eight: "Better Call Saul",
+                    nine: "4 Days Out",
+                    ten: "Over",
+                    eleven: "Mandala",
+                    twelve: "Phoenix",
+                    thirteen: "ABQ"
+                }
+            },
+            three: {
+                year: 2010,
+                episodes: {
+                    one: "No Más",
+                    two: "Caballo sin Nombre",
+                    three: "I.F.T.",
+                    four: "Green Light",
+                    five: "Más",
+                    six: "Sunset",
+                    seven: "One Minute",
+                    eight: "I See You",
+                    nine: "Kafkaesque",
+                    ten: "Fly",
+                    eleven: "Abiquiu",
+                    twelve: "Half Measures",
+                    thirteen: "Full Measure"
+                }
+            },
+            four: {
+                year: 2011,
+                episodes: {
+                    one: "Box Cutter",
+                    two: "Thirty-Eight Snub",
+                    three: "Open House",
+                    four: "Bullet Points",
+                    five: "Shotgun",
+                    six: "Cornered",
+                    seven: "Problem Dog",
+                    eight: "Hermanos",
+                    nine: "Bug",
+                    ten: "Salud",
+                    eleven: "Crawl Space",
+                    twelve: "End Times",
+                    thirteen: "Face Off"
+                }
+            },
+            five: {
+                year: 2012-2013,
+                episodes: {
+                    one: "Live Free or Die",
+                    two: "Madrigal",
+                    three: "Hazard Pay",
+                    four: "Fifty-One",
+                    five: "Dead Freight",
+                    six: "Buyout",
+                    seven: "Say My Name",
+                    eight: "Gliding Over All",
+                    nine: "Blood Money",
+                    ten: "Buried",
+                    eleven: "Confessions",
+                    twelve: "Rabid Dog",
+                    thirteen: "To'hajiilee",
+                    fourteen: "Ozymandias",
+                    fifteen: "Granite State",
+                    sixteen: "Felina"
+                }
+            },
+        }
+    },
+    {
+        name: "Better Call Saul",
+        synopsis: "Struggling lawyer Jimmy McGill tries to leave his seedy past behind him, but old habits die hard when a big opportunity presents itself.",
+        img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/S/pv-target-images/fe1c0138b7b0e05ea22a711f44e57cd80cfbaea30745c425b7043d786ba66cd1.jpg",
+        genre: ["crime tv shows", "tv dramas"],
+        seasons: {
+            one: {
+                year: 2015,
+                episodes: {
+                    one: "Uno",
+                    two: "Mijo",
+                    three: "Nacho",
+                    four: "Hero",
+                    five: "Alpine Shepherd Boy",
+                    six: "Five-O",
+                    seven: "Bingo",
+                    eight: "RICO",
+                    nine: "Pimento",
+                    ten: "Marco"
+                }
+            },
+            two: {
+                year: 2016,
+                episodes: {
+                    one: "Switch",
+                    two: "Cobbler",
+                    three: "Amarillo",
+                    four: "Gloves Off",
+                    five: "Rebecca",
+                    six: "Bali Ha'i",
+                    seven: "Inflatable",
+                    eight: "Fifi",
+                    nine: "Nailed",
+                    ten: "Klick"
+                }
+            },
+            three: {
+                year: 2017,
+                episodes: {
+                    one: "Mabel",
+                    two: "Witness",
+                    three: "Sunk Costs",
+                    four: "Sabrosito",
+                    five: "Chicanery",
+                    six: "Off Brand",
+                    seven: "Expenses",
+                    eight: "Slip",
+                    nine: "Fall",
+                    ten: "Lantern"
+                }
+            },
+            four: {
+                year: 2018,
+                episodes: {
+                    one: "Smoke",
+                    two: "Breathe",
+                    three: "Something Beautiful",
+                    four: "Talk",
+                    five: "Quite a Ride",
+                    six: "Piñata",
+                    seven: "Something Stupid",
+                    eight: "Coushatta",
+                    nine: "Wiedersehen",
+                    ten: "Winner"
+                }
+            },
+            five: {
+                year: 2020,
+                episodes: {
+                    one: "Magic Man",
+                    two: "50% Off",
+                    three: "The Guy for This",
+                    four: "Namaste",
+                    five: "Dedicado a Max",
+                    six: "Wexler v. Goodman",
+                    seven: "JMM",
+                    eight: "Bagman",
+                    nine: "Bad Choice Road",
+                    ten: "Something Unforgivable"
+                }
+            },
+            six: {
+                year: 2022,
+                episodes: {
+                    one: "Wine and Roses",
+                    two: "Carrot and Stick",
+                    three: "Rock and Hard Place",
+                    four: "Hit and Run",
+                    five: "Black and Blue",
+                    six: "Axe and Grind",
+                    seven: "Plan and Execution",
+                    eight: "Point and Shoot",
+                    nine: "Fun and Games",
+                    ten: "Nippy",
+                    eleven: "Breaking Bad",
+                    twelve: "Waterworks",
+                    thirteen: "Saul Gone"
+                }
+            },
+        }
+    },
+    {
+        name: "Ozark",
+        synopsis: "After his business partner cheats a dangerous client, financial adviser Marty must devise a radical plan to save the lives of himself and his family.",
+        img: "https://static.wikia.nocookie.net/listofdeaths/images/9/99/Ozark.jpg/revision/latest?cb=20220527131130",
+        genre: ["crime tv shows", "tv dramas", "tv thtillers"],
+        seasons: {
+            one: {
+                year: 2017,
+                episodes: {
+                    one: "Sugarwood",
+                    two: "Blue Cat",
+                    three: "My Dripping Sleep",
+                    four: "Tonight We Improvise",
+                    five: "Ruling Days",
+                    six: "Book of Ruth",
+                    seven: "Nest Box",
+                    eight: "Kaleidoscope",
+                    nine: "Coffee, Black",
+                    ten: "The Toll"
+                }
+            },
+            two: {
+                year: 2018,
+                episodes: {
+                    one: "Reparations",
+                    two: "The Precious Blood of Jesus",
+                    three: "Once a Langmore...",
+                    four: "Stag",
+                    five: "Game Day",
+                    six: "Outer Darkness",
+                    seven: "One Way Out",
+                    eight: "The Big Sleep",
+                    nine: "The Badger",
+                    ten: "The Gold Coast"
+                }
+            },
+            three: {
+                year: 2020,
+                episodes: {
+                    one: "Wartime",
+                    two: "Civil Union",
+                    three: "Kevin Cronin Was Here",
+                    four: "Boss Fight",
+                    five: "It Came from Michoacán",
+                    six: "Su Casa Es Mi Casa",
+                    seven: "In Case of Emergency",
+                    eight: "BFF",
+                    nine: "Fire Pink",
+                    ten: "All In"
+                }
+            },
+            four: {
+                year: 2022,
+                episodes: {
+                    one: "The Beginning of the End",
+                    two: "Let the Great World Spin",
+                    three: "City on the Make",
+                    four: "Ace Deuce",
+                    five: "Ellie",
+                    six: "Sangre Sobre Todo",
+                    seven: "Sanctified",
+                    eight: "The Cousin of Death",
+                    nine: "Pick a God and Pray",
+                    ten: "You're the Boss",
+                    eleven: "Pound of Flesh and Still Kickin'",
+                    twelve: "Trouble the Water",
+                    thirteen: "Mud",
+                    fourteen: "A Hard Way to Go"
+                }
+            }
+        }
+    },
+    {
+        name: "Peaky Blinders",
+        synopsis: "Ambitious gang leader Thomas Shelby recognizes an opportunity to move up in the world thanks to a missing crate of guns.",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d7590f135861167.61ef6fb49f8e4.jpg",
+        genre: ["british", "crime tv shows", "tv dramas", "period pieces"],
+        seasons: {
+            one: {
+                year: 2014,
+                episodes: {
+                    one: "1.1",
+                    two: "1.2",
+                    three: "1.3",
+                    four: "1.4",
+                    five: "1.5",
+                    six: "1.6"
+                }
+            },
+            two: {
+                year: 2014,
+                episodes: {
+                    one: "2.1",
+                    two: "2.2",
+                    three: "2.3",
+                    four: "2.4",
+                    five: "2.5",
+                    six: "2.6",
+                }
+            },
+            three: {
+                year: 2016,
+                episodes: {
+                    one: "3.1",
+                    two: "3.2",
+                    three: "3.3",
+                    four: "3.4",
+                    five: "3.5",
+                    six: "3.6"
+                }
+            },
+            four: {
+                year: 2017,
+                episodes: {
+                    one: "The Noose",
+                    two: "Heathens",
+                    three: "Blackbird",
+                    four: "Dangerous",
+                    five: "The Duel",
+                    six: "The Company"
+                }
+            },
+            five: {
+                year: 2019,
+                episodes: {
+                    one: "Black Tuesday",
+                    two: "Black Cats",
+                    three: "Strategy",
+                    four: "The Loop",
+                    five: "The Shock",
+                    six: "Mr. Jones"
+                }
+            },
+            six: {
+                year: 2022,
+                episodes: {
+                    one: "Black Day",
+                    two: "Black Shirt",
+                    three: "Gold",
+                    four: "Sapphire",
+                    five: "The Road to Hell",
+                    six: "Lock and Key"
+                }
+            },
+        }
+    },
+]
