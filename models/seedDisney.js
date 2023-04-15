4ffc6173c6145a50fbb95785200302d908788c53
armullen <amber.r.mullen@gmail.com>
2023-04-15 14:22:38 -0500

    fixed episode data in seed

--- models/seedDisney.js
+++ models/seedDisney.js
@@ -2,196 +2,195 @@ module.exports = [
     {
         name: "The Mandalorian",
         synopsis: "After the fall of the Empire, a lone Mandalorian makes his way through the lawless galaxy with his foundling, Grogu.",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzehddek1Xj_1AniR2aPrOKlNeh4nOprmExw&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzehddek1Xj_1AniR2aPrOKlNeh4nOprmExw&usqp=CAU",
         genre: ["science-fiction", "action-adventure"],
-        seasons: {
-            one: {
+        seasons: [
+             {
                 year: 2019,
-                episodes: {
-                    one: "Chapter 1: The Mandalorian",
-                    two: "Chapter 2: The Child",
-                    three: "Chapter 3: The Sin",
-                    four: "Chapter 4: Sancuary",
-                    five: "Chapter 5: The Ginslinger",
-                    six: "Chapter 6: The Prisoner",
-                    seven: "Chapter 7: The Reckoning",
-                    eight: "Chapter 8: Redemption",
-                }
+                episodes: [
+                    "Chapter 1: The Mandalorian",
+                    "Chapter 2: The Child",
+                    "Chapter 3: The Sin",
+                    "Chapter 4: Sancuary",
+                    "Chapter 5: The Ginslinger",
+                    "Chapter 6: The Prisoner",
+                    "Chapter 7: The Reckoning",
+                    "Chapter 8: Redemption",
+                ]
             },
-            two: {
+             {
                 year: 2020,
-                episodes: {
-                    one: "Chapter 9: The Marshal",
-                    two: "Chapter 10: The Passenger",
-                    three: "Chapter 11: The Heiress",
-                    four: "Chapter 12: The Siege",
-                    five: "Chapter 13: The Jedi",
-                    six: "Chapter 14: The Tragedy",
-                    seven: "Chapter 15: The Believer",
-                    eight: "Chapter 16: The Rescue",
-                }
+                episodes: [
+                    "Chapter 9: The Marshal",
+                    "Chapter 10: The Passenger",
+                    "Chapter 11: The Heiress",
+                    "Chapter 12: The Siege",
+                    "Chapter 13: The Jedi",
+                    "Chapter 14: The Tragedy",
+                    "Chapter 15: The Believer",
+                    "Chapter 16: The Rescue",
+                ]
             }
 
-        }
+        ]
     },
     {
 
-        
+
         name: "The Simpsons",
         synopsis: "This long-running animated comedy focuses on the eponymous family in the town of Springfield in an unnamed U.S. state. The head of the Simpson family, Homer, is not a typical family man. A nuclear-plant employee, he does his best to lead his family but often finds that they are leading him. The family includes loving, blue-haired matriarch Marge, troublemaking son Bart, overachieving daughter Lisa and baby Maggie. Other Springfield residents include the familys religious neighbour, Ned Flanders, family physician Dr Hibbert, Moe the bartender and police chief Clancy Wiggum.",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Roz52iTVPuxUIiuzMUQxej8JIZM43SXCOA&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Roz52iTVPuxUIiuzMUQxej8JIZM43SXCOA&usqp=CAU",
         genre: ["Comedy", "Animation"],
-        seasons: {
-            one: {
+        seasons: [
+           {
                 year: 1989,
-                episodes: {
-                    one: "Simpsons Roasting on an Open Fire",
-                    two: "Bart the Genius",
-                    three: "Homers Odyssey",
-                    four: "Theres No Disgrace Like Home",
-                    five: "Bart the General",
-                    six: "Moaning Lisa",
-                    seven: "The Call of the Simpsons",
-                    eight: "The Telltale Head",
-                    nine: "Life on the Fast Lane",
-                    ten: "Homers Night Out"
-                }
+                episodes: [
+                    "Simpsons Roasting on an Open Fire",
+                    "Bart the Genius",
+                    "Homers Odyssey",
+                    "Theres No Disgrace Like Home",
+                    "Bart the General",
+                    "Moaning Lisa",
+                    "The Call of the Simpsons",
+                    "The Telltale Head",
+                    "Life on the Fast Lane",
+                    "Homers Night Out"
+                ]
             },
-            two: {
+            {
                 year: 1990,
-                episodes: {
-                    one: "Bart Gets an F",
-                    two: "Simpson and Delilah",
-                    three: "Treehouse of Horror",
-                    four: "Two Cars in Every Garage and Three Eyes on Every Fish",
-                    five: "Dancin Homer",
-                    six: "Dead Putting Society",
-                    seven: "Bart vs. Thanksgiving",
-                    eight: "Bart the Daredevil",
-                    nine: "Itchy & Scratchy & Marge",
-                    ten: "Bart Gets Hit by a Car"
-                }
+                episodes: [
+                    "Bart Gets an F",
+                    "Simpson and Delilah",
+                    "Treehouse of Horror",
+                    "Two Cars in Every Garage and Three Eyes on Every Fish",
+                    "Dancin Homer",
+                    "Dead Putting Society",
+                    "Bart vs. Thanksgiving",
+                    "Bart the Daredevil",
+                    "Itchy & Scratchy & Marge",
+                    "Bart Gets Hit by a Car"
+                ]
             }
-            
-        }
+
+        ]
     },
     {
 
         name: "Miraculous - Tales of Ladybug & Cat Noir",
         synopsis: "Marinette seems to be a normal teenage girl -- she dreams of becoming a fashion designer, is a bit awkward and has a crush on a classmate. But she has a secret other teenagers dont have -- she lives a double life as a crime-fighting superhero known as Ladybug. She uses her superpowers to help protect her native Paris from supervillain Hawk Moth. She is assisted, reluctantly, by fellow hero Cat Noir, even though she insists she doesnt need him -- a stance she might not take if she knew his alter ego is her crush, Adrien. Together, Ladybug and Cat Noir help keep Paris safe from Hawk Moth and other villains.",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2cw9WCgsE1iKvh8emHW2p66PDIOue2xSong&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2cw9WCgsE1iKvh8emHW2p66PDIOue2xSong&usqp=CAU",
         genre: ["Romance", "Animation", "Fantasy", "Super Hero", "Action-Adventure"],
-        seasons: {
-            one: {
+        seasons: [
+            {
                 year: 2015,
-                episodes: {
-                    one: "Stormy Weather",
-                    two: "The Bubbler",
-                    three: "Timebreaker",
-                    four: "The Pharaoh",
-                    five: "Mr. Pigeon",
-                    six: "Lady Wifi",
-                    seven: "The Evillustrator",
-                    eight: "Rogercop",
-                    nine: "Horrificator",
-                    ten: "Dark Cupid"
-                }
+                episodes: [
+                    "Stormy Weather",
+                    "The Bubbler",
+                    "Timebreaker",
+                    "The Pharaoh",
+                    "Mr. Pigeon",
+                    "Lady Wifi",
+                    "The Evillustrator",
+                    "Rogercop",
+                    "Horrificator",
+                    "Dark Cupid"
+                ]
             },
-            two: {
+            {
                 year: 2016,
-                episodes: {
-                    one: "The Collector",
-                    two: "Prime Queen",
-                    three: "Glaciator",
-                    four: "Despair Bear",
-                    five: "Troublemaker",
-                    six: "Gigantitan",
-                    seven: "Riposte",
-                    eight: "Befana",
-                    nine: "Frightningale",
-                    ten: "Gorizilla"
-                }
+                episodes: [
+                    "The Collector",
+                    "Prime Queen",
+                    "Glaciator",
+                    "Despair Bear",
+                    "Troublemaker",
+                    "Gigantitan",
+                    "Riposte",
+                    "Befana",
+                    "Frightningale",
+                    "Gorizilla"
+                ]
             }
-
-        }
+        ]
     },
     {
         name: "Once Upon A Time",
         synopsis: "Once Upon a Time tells the story of a new world, one in which fairy-tale legends and modern life collide. Emma Swan (Jennifer Morrison) is comfortable in her life as a bail bonds collector when Henry -- the child she gave up a decade earlier -- suddenly shows up. He is convinced that she is the daughter of Snow White and Prince Charming, who sent her away before the Evil Queen could cast a spell, freezing the fairytale world in time and bringing them to present-day Storybrooke, Maine. After taking Henry home, Emma decides to stay in the town to keep an eye on him, and she discovers he may not be wrong after all.",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkEzvSgbkDNpdCJqW-DszjcBUIfu0KGyFpyw&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkEzvSgbkDNpdCJqW-DszjcBUIfu0KGyFpyw&usqp=CAU",
         genre: ["Soap Opera", "Melodrama", "Fantasy", "Action-Adventure"],
-        seasons: {
-            one: {
+        seasons: [
+            {
                 year: 2011,
-                episodes: {
-                    one: "Pilot",
-                    two: "The Thing You Love Most",
-                    three: "Snow Falls",
-                    four: "The Price of Gold",
-                    five: "That Still Small Voice",
-                    six: "The Shepherd",
-                    seven: "The Heart is a Lonely Hunter",
-                    eight: "Desperate Souls",
-                    nine: "True North",
-                    ten: "7:15 A.M."
-                }
+                episodes: [
+                    "Pilot",
+                    "The Thing You Love Most",
+                    "Snow Falls",
+                    "The Price of Gold",
+                    "That Still Small Voice",
+                    "The Shepherd",
+                    "The Heart is a Lonely Hunter",
+                    "Desperate Souls",
+                    "True North",
+                    "7:15 A.M."
+                ]
             },
-            two: {
+            {
                 year: 2012,
-                episodes: {
-                    one: "Broken",
-                    two: "We are Both",
-                    three: "Lady of the Lake",
-                    four: "The Crocodile",
-                    five: "The Doctor",
-                    six: "Tallahassee",
-                    seven: "Child of the Moon",
-                    eight: "Into the Deep",
-                    nine: "Queen of Hearts",
-                    ten: "The Cricket Game"
-                }
+                episodes: [
+                    "Broken",
+                    "We are Both",
+                    "Lady of the Lake",
+                    "The Crocodile",
+                    "The Doctor",
+                    "Tallahassee",
+                    "Child of the Moon",
+                    "Into the Deep",
+                    "Queen of Hearts",
+                    "The Cricket Game"
+                ]
             }
+        ]
 
-        }
     },
     {
         name: "Phineas and Ferb",
         synopsis: "Phineas Flynn and his stepbrother, Ferb Fletcher, are determined to make every day of their summer vacation fun. They try building a full-scale roller coaster, becoming musicians, building a backyard beach, and traveling back in time to meet dinosaurs. Their family pet, Perry the Platypus, doubles as a secret agent and battles evil. Meanwhile, older sister Candace is tattling on them to Mom and Dad, though she can never seem to catch them in the act.",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtwwlhiyTuxAuObGu9Ag8cmZMA37iLXuYjxA&usqp=CAU",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtwwlhiyTuxAuObGu9Ag8cmZMA37iLXuYjxA&usqp=CAU",
         genre: ["Comedy", "Musical", "Kids", "Action-Adventure", "Anamation"],
-        seasons: {
-            one: {
+        seasons: [
+            {
                 year: 2007,
-                episodes: {
-                    one: "Rollercoaster/Candace Loses Her Head",
-                    two: "The Fast and the Phineas/Lawn Gnome Beach Party of Terror!",
-                    three: "The Magnificent Few/S-Winter",
-                    four: "Are You My Mummy?/Flop Starz",
-                    five: "Raging Bully / Lights, Candace, Action!",
-                    six: "Get That Bigfood Outa My Face! / Tree to Get Ready",
-                    seven: "Its About Timee!",
-                    eight: "Jerk De Soleil / Toy to the World",
-                    nine: "One Good Scaare Ought To Do It!",
-                    ten: "A Hard Days Knight/ I, Brobot"
-                }
+                episodes: [
+                    "Rollercoaster/Candace Loses Her Head",
+                    "The Fast and the Phineas/Lawn Gnome Beach Party of Terror!",
+                    "The Magnificent Few/S-Winter",
+                    "Are You My Mummy?/Flop Starz",
+                    "Raging Bully / Lights, Candace, Action!",
+                    "Get That Bigfood Outa My Face! / Tree to Get Ready",
+                    "Its About Timee!",
+                    "Jerk De Soleil / Toy to the World",
+                    "One Good Scaare Ought To Do It!",
+                    "A Hard Days Knight/ I, Brobot"
+                ]
             },
-            two: {
+            {
                 year: 2008,
-                episodes: {
-                    one: "The Lake Nose Monster",
-                    two: "Interview with a Platypus / Tip of the Day",
-                    three: "Attack of the 50-Foot Sister / Backyard Aquarium",
-                    four: "Day of the Living Gelatin! / Elementary, My Dear Stacy",
-                    five: "Dont Even Blink / Chez Platypus",
-                    six: "Perry Lays an Egg / Gaming the System",
-                    seven: "The Chronicles of Meap",
-                    eight: "Thaddeus and Thor / De Plane! De Plane!",
-                    nine: "Lets Take a Quiz / At the Car Wash",
-                    ten: "Oh, There you Are, Perry / Swiss Family Phineas"
-                }
+                episodes: [
+                    "The Lake Nose Monster",
+                    "Interview with a Platypus / Tip of the Day",
+                    "Attack of the 50-Foot Sister / Backyard Aquarium",
+                    "Day of the Living Gelatin! / Elementary, My Dear Stacy",
+                    "Dont Even Blink / Chez Platypus",
+                    "Perry Lays an Egg / Gaming the System",
+                    "The Chronicles of Meap",
+                    "Thaddeus and Thor / De Plane! De Plane!",
+                    "Lets Take a Quiz / At the Car Wash",
+                    "Oh, There you Are, Perry / Swiss Family Phineas"
+                ]
             }
 
-        }
+        ]
     }
-    
+
 ]
